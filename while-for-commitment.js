01530360d182e6a00b2a39f9de519144720cb4ea
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-08 03:54:30 -0800

    for loop commitment js done

---
+++ while-for-commitment.js
@@ -0,0 +1,24 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+// let count = 1;
+
+// while(count<=60){
+//     console.log(count);
+//     count++;
+// }
+
+
+/***
+
+" for loop using I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+
+for (let count = 1; count<=60; count++){
+    console.log(count);
+}
