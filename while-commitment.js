42a723c11b448a81ea14e7544e97291bdee55d80
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-08 01:18:36 -0800

    while-commitment js done

---
+++ while-commitment.js
@@ -0,0 +1,12 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+let count = 1;
+
+while(count<=60){
+    console.log(count);
+    count++;
+}
